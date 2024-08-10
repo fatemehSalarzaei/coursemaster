9856dc866d4fa363db712bc1a4807ff4912576b9
fatemeh salarzaei <salarzaei96@gmail.com>
2024-08-11 00:12:06 +0330

    Design Notification model with global and user-specific notifications
    - Created Notification model with fields for message, is_global, and user
    - Configured admin interface to manage notifications
    Design InstructorNotification model for notifications from instructors to students
    
    - Created InstructorNotification model to handle notifications from instructors to specific students or all students in a course
    - Added related models for Course and Enrollment
    - Configured admin interface to manage instructor notifications

--- core/Notifications/admin.py
+++ core/Notifications/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Notification , InstructorNotification
 
-# Register your models here.
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('message', 'is_global', 'user', 'created_at')
+    list_filter = ('is_global', 'created_at')
+    search_fields = ('message', 'user__username')
+
+class InstructorNotificationAdmin(admin.ModelAdmin):
+    list_display = ('message', 'is_global', 'course', 'specific_student', 'created_at')
+    list_filter = ('is_global', 'created_at', 'course')
+    search_fields = ('message', 'course__title', 'specific_student__username')
+
+admin.site.register(Notification, NotificationAdmin)
+admin.site.register(Notification, InstructorNotificationAdmin)
--- core/Notifications/models.py
+++ core/Notifications/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+from Courses.models import Course
+
+class Notification(models.Model):
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_global = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='notifications')
+
+    def __str__(self):
+        if self.is_global:
+            return f"Global Notification: {self.message[:50]}"
+        return f"Notification for {self.user.username}: {self.message[:50]}"
+    
+class InstructorNotification(models.Model):
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_global = models.BooleanField(default=False)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True, related_name='notifications')
+    specific_student = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='specific_notifications')
+
+    def __str__(self):
+        if self.is_global:
+            return f"Global Notification for Course {self.course.title if self.course else 'All Courses'}: {self.message[:50]}"
+        return f"Notification for {self.specific_student.username} in Course {self.course.title if self.course else 'None'}: {self.message[:50]}"
