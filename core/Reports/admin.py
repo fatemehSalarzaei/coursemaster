fb6d60cc2ceb10fd40b98670c55f0638ec19024c
fatemeh salarzaei <salarzaei96@gmail.com>
2024-08-11 00:30:09 +0330

    Add  reporting
    - Created models for Report, ExamAnalysis, and NotificationAnalysis for reporting and analytics
    - Updated admin interface to manage and view reports and analyses

--- core/Reports/admin.py
+++ core/Reports/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Report, ExamAnalysis, NotificationAnalysis
 
-# Register your models here.
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ('report_type', 'generated_at')
+    search_fields = ('report_type',)
+
+class ExamAnalysisAdmin(admin.ModelAdmin):
+    list_display = ('exam', 'average_score', 'highest_score', 'lowest_score', 'generated_at')
+    search_fields = ('exam__title',)
+
+class NotificationAnalysisAdmin(admin.ModelAdmin):
+    list_display = ('notification', 'total_sent', 'total_delivered', 'total_failed', 'generated_at')
+    search_fields = ('notification__message',)
+
+admin.site.register(Report, ReportAdmin)
+admin.site.register(ExamAnalysis, ExamAnalysisAdmin)
+admin.site.register(NotificationAnalysis, NotificationAnalysisAdmin)
--- core/Reports/models.py
+++ core/Reports/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from Assignments.models import Exam
+from  Notifications.models import InstructorNotification
+
+class Report(models.Model):
+    generated_at = models.DateTimeField(auto_now_add=True)
+    report_type = models.CharField(max_length=255)
+    data = models.JSONField()  # برای ذخیره داده‌های گزارش به صورت JSON
+
+    def __str__(self):
+        return f"{self.report_type} Report - {self.generated_at}"
+
+class ExamAnalysis(models.Model):
+    exam = models.ForeignKey( Exam, on_delete=models.CASCADE)
+    average_score = models.FloatField()
+    highest_score = models.FloatField()
+    lowest_score = models.FloatField()
+    generated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Analysis for {self.exam.title} - {self.generated_at}"
+
+class NotificationAnalysis(models.Model):
+    notification = models.ForeignKey(InstructorNotification, on_delete=models.CASCADE)
+    total_sent = models.IntegerField()
+    total_delivered = models.IntegerField()
+    total_failed = models.IntegerField()
+    generated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Analysis for Notification {self.notification.id} - {self.generated_at}"
